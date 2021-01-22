b7dcec0481b551d863b776902bada90566e4bdad
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2021-01-22 13:00:43 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+        // defaults to ./src, Here the application starts executing and webpack starts bundling
+    entry: "./src/index.js",
+    output: {
+            // the filename template for entry chunks, voi olla mikä tahansa
+        filename: "bundle.js",
+            // the target directory for all output files
+            // must be an absolute path (use the Node.js path module)
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    	//Jos käytät serveriä
+    devServer: {
+        port: 8080,
+        contentBase: path.resolve(__dirname, 'dist'),
+        hot: true
+    },
+}
